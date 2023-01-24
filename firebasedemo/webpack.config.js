9c6632e50f4dcdbdb1473d7e78849ad1271a7ebf
jkvjerin <jkvjerin@gmail.com>
2023-01-24 09:13:38 +0530

    WIP ECOMM

---
+++ firebasedemo/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+var config = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devtool: 'eval-source-map',
+  watch: true,
+};
+module.exports = config;
+
+// module.exports = (env, argv) => {
+//   if (argv.mode === 'development') {
+//     config.devtool = 'source-map';
+//   }
+
+//   if (argv.mode === 'production') {
+//     //...
+//   }
+
+//   return config;
+// };
