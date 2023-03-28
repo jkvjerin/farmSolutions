0b0bb8eb46b427d4c008c9c25083c918392a95c7
jkvjerin <jkvjerin@gmail.com>
2023-03-28 15:28:12 +0530

    28 mar 23 evening commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const TerserWebpackPlugin = require("terser-webpack-plugin");
+
 //const modepro = require('production')
 // import { modepro } from '.\webpack-production-config.jsproduction'
  const config = {
@@ -7,10 +11,36 @@ const path = require('path')
     main :'./src/index.js',
     addproduct:'./src/main_addproduct.js',
     showProduct :'./src/showProduct.js',
+    // ar: "./src/index.ts",
   }
+  // ,
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.ts?$/,
+  //       use: "ts-loader",
+  //       exclude: /node_modules/,
+  //     },
+  //     {
+  //       test: /\.css$/i,
+  //       use: ["style-loader", "css-loader"],
+  //     },
+  //     {
+  //       test: /\.(png|jpe?g|gif|glb)$/i,
+  //       use: [
+  //         {
+  //           loader: "url-loader",
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // resolve: {
+  //   extensions: [".tsx", ".ts", ".js", ".css", ".jpg"],
+  // }
   ,
-  //  mode: 'development',
-  mode: 'production',
+   mode: 'development',
+  // mode: 'production',
   // define output point
   output: {
     path: path.resolve(__dirname, 'dist'),
